79f4bd9b4fafe4bc8b38f4f1dbd2a32295099479
jorge machado <jrms_jorgesilva@hotmail.com>
2022-07-08 22:27:43 -0300

    docs: documentation updated with tasks to improve the API service

---
+++ docs/tasks.md
@@ -0,0 +1,7 @@
+# TASKS
+
+## - [] Criar views
+## - [] Setup de styles com scss e 7 - 1 structure
+## - [] TDD com jest e pasta de testes unitarios
+## - [] Politica de Cookies e scritps 
+## - []  
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const path = require('path');
 const cors = require("cors");
 const axios = require("axios");
 const mongooseConnect = require("./database/mongo.js");
-const personsRoutes = require('./routes/personsRoute');
+const personsRoutes = require("./routes/personsRoute");
 
 // server
 const server = express();
@@ -13,7 +14,13 @@ server.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // API routes
-server.use('/persons', personsRoutes);
+server.all("*", (req, res) => {
+  console.log("authentication");
+});
+
+server.use("/persons", personsRoutes);
+
+server.use(express.static(path.join(__dirname, './index.html')));
 
 mongooseConnect(server);
 server.listen(PORT, () => console.log(`server is running on the port ${PORT}`));
