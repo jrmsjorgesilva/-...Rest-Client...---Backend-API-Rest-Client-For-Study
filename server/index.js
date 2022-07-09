224fbbfddc5b6b87c503c74f144606b1d9ed046b
jorge machado <jrms_jorgesilva@hotmail.com>
2022-07-09 17:54:53 -0300

    views and tests already put together

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const cors = require("cors");
 const axios = require("axios");
 const mongooseConnect = require("./database/mongo.js");
 const personsRoute = require("./routes/personsRoute");
+const indexView = require('../views/js/index');
 
 // server
 const server = express();
@@ -17,7 +18,7 @@ server.use(express.json());
 // use
 server.use("/persons", personsRoute);
 
-server.use(express.static(path.join(__dirname, './index.html')));
+server.use(express.static(path.join(__dirname, "./index.html")));
 
 // API routes
 // server.all("*", (req, res) => {
@@ -29,11 +30,12 @@ server.use(express.static(path.join(__dirname, './index.html')));
 // });
 
 // Views
-server.set('view engine', 'ejs');
+server.set("view engine", "ejs");
 
-server.get('/', (req, res) => {
-    res.render('index');
-})
+server.get("/", (req, res) => {
+//   indexView
+  res.render("index", indexView);
+});
 
 mongooseConnect(server);
 server.listen(PORT, () => console.log(`server is running on the port ${PORT}`));
---
+++ views/js/index.js
@@ -0,0 +1,40 @@
+const title = "Boilerplate | JM Softwares";
+const mainContent = "Main Content";
+const asideContent = "Sidebar Content";
+const SEO = {
+  ogImage: "image.jpg",
+  content: "content",
+  keywords: ["web design", "internship", "bootscamp"],
+  website: "www.website.com.br",
+};
+
+{/* <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="pingback" href="https://blog.logrocket.com/xmlrpc.php">
+<title>How to make HTTP requests with Axios - LogRocket Blog</title>
+<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+<meta name="description" content="Axios has become undeniably popular among frontend developers. We&#039;ll examine the key reasons that have contributed to its rapid rise." />
+<link rel="canonical" href="https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="How to make HTTP requests with Axios - LogRocket Blog" />
+<meta property="og:description" content="Axios has become undeniably popular among frontend developers. We&#039;ll examine the key reasons that have contributed to its rapid rise." />
+<meta property="og:url" content="https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/" />
+<meta property="og:site_name" content="LogRocket Blog" />
+<meta property="article:published_time" content="2021-01-26T17:00:37+00:00" />
+<meta property="article:modified_time" content="2021-11-29T22:23:37+00:00" />
+<meta property="og:image" content="https://blog.logrocket.com/wp-content/uploads/2021/01/how-to-make-http-requests-axios-nocdn.png" />
+<meta property="og:image:width" content="730" />
+<meta property="og:image:height" content="487" />
+<meta property="og:image:type" content="image/png" />
+<meta name="author" content="Faraz Kelhini" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:creator" content="@https://twitter.com/FarazKelhini" />
+<meta name="twitter:label1" content="Written by" />
+<meta name="twitter:data1" content="Faraz Kelhini" />
+<meta name="twitter:label2" content="Est. reading time" />
+<meta name="twitter:data2" content="13 minutes" />
+<script type="application/ld+json" class="yoast-schema-graph">{"@context":"https://schema.org","@graph":[{"@type":"WebSite","@id":"https://blog.logrocket.com/#website","url":"https://blog.logrocket.com/","name":"LogRocket Blog","description":"For Frontend Developers and Web App Engineers","potentialAction":[{"@type":"SearchAction","target":{"@type":"EntryPoint","urlTemplate":"https://blog.logrocket.com/?s={search_term_string}"},"query-input":"required name=search_term_string"}],"inLanguage":"en-US"},{"@type":"ImageObject","inLanguage":"en-US","@id":"https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/#primaryimage","url":"https://blog.logrocket.com/wp-content/uploads/2021/01/how-to-make-http-requests-axios-nocdn.png","contentUrl":"https://blog.logrocket.com/wp-content/uploads/2021/01/how-to-make-http-requests-axios-nocdn.png","width":730,"height":487,"caption":"How to Make HTTP Requests with Axios"},{"@type":"WebPage","@id":"https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/#webpage","url":"https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/","name":"How to make HTTP requests with Axios - LogRocket Blog","isPartOf":{"@id":"https://blog.logrocket.com/#website"},"primaryImageOfPage":{"@id":"https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/#primaryimage"},"datePublished":"2021-01-26T17:00:37+00:00","dateModified":"2021-11-29T22:23:37+00:00","author":{"@id":"https://blog.logrocket.com/#/schema/person/0b2c486552e06560b90743969ae3d50d"},"description":"Axios has become undeniably popular among frontend developers. We'll examine the key reasons that have contributed to its rapid rise.","breadcrumb":{"@id":"https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/#breadcrumb"},"inLanguage":"en-US","potentialAction":[{"@type":"ReadAction","target":["https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/"]}]},{"@type":"BreadcrumbList","@id":"https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/#breadcrumb","itemListElement":[{"@type":"ListItem","position":1,"name":"Home","item":"https://blog.logrocket.com/"},{"@type":"ListItem","position":2,"name":"How to make HTTP requests with Axios"}]},{"@type":"Person","@id":"https://blog.logrocket.com/#/schema/person/0b2c486552e06560b90743969ae3d50d","name":"Faraz Kelhini","image":{"@type":"ImageObject","inLanguage":"en-US","@id":"https://blog.logrocket.com/#/schema/person/image/","url":"https://secure.gravatar.com/avatar/25e546a6e2e1903d2554412d24aa57ee?s=96&d=mm&r=g","contentUrl":"https://secure.gravatar.com/avatar/25e546a6e2e1903d2554412d24aa57ee?s=96&d=mm&r=g","caption":"Faraz Kelhini"},"description":"JavaScript developer.","sameAs":["https://eloux.com/","https://twitter.com/https://twitter.com/FarazKelhini"],"url":"https://blog.logrocket.com/author/farazkelhini/"}]}</script> */}
+
+
+module.exports = { title, mainContent, asideContent, SEO };
