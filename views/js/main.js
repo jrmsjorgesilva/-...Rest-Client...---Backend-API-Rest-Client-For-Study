04d49594ab5d7102be83b1af2e4264e208b6ad85
jorge machado <jrms_jorgesilva@hotmail.com>
2022-07-11 13:27:13 -0300

    feat: added js scripts

--- views/js/main.js
+++ views/js/main.js
@@ -1,1 +1,18 @@
-console.log('main');
+
+
+fetch('http://localhost:8000/persons').then((res) => {
+    res.json().then((data) => {
+        data.map(item => {
+            item.firstName ? console.log(item.firstName + ' ' + item.lastName) : '';
+        })
+    })
+})
+
+const btnSubmit = document.querySelector('form__btn');
+
+console.log(btnSubmit);
+
+btnSubmit.addEventListener('submit', (e) => {
+    e.prenventDefault();
+    console.log('prevented');
+})
