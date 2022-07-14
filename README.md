fde9a83c3fce6f00bad1709a6ff607391deda41d
jorge machado <jrms_jorgesilva@hotmail.com>
2022-07-14 11:49:33 -0300

    docs: adicionadas informações ao readme do projeto

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # API Rest Boilerplate
 
 ## A case for study of rest API's implementation - Development of a custom client rest
+
+## To setup the project use 
+
+`npm install` or `yarn install` and run it on you localhost
+
+Setup environment variables to your local development
+
+<img src="/views/assets/img/demo/admin_rest_client.png" alt="Demo"/>
+
+# Projeto em andamento Tasks a serem implementadas
+
+## - [x] Criar views
+## - [x] Template EJS / HBS (opted by handlebars)
+## - [x] SCSS 7 - 1 structure folder
+## - [x] Setup de styles com scss e 7 - 1 structure
+## - [] Test with jest (to be implemented)
+## - [] Cookies policy 
+## - [x] 404 not found page
+## - [] Encryption and security
+## - [] Authentication
+## - [] Verify if the data already exists in the database on the POST method
+## - [] Backup automated routine
+## - [X] Integration Mercado Pago
+## - [X] Integration Stripe
+## - [] Integration pagar.me
+## - [] Integration pagseguro
+## - [] Integration picpay
+## - [] Integration wirecard
+## - [] Integration iugu
+## - [] Integration PIX
+## - [] Integration nota fiscal eletronica
+## - [] Google Ads support
+## - [] Google API
--- routes/api/mercadoPagoRoute.js
+++ routes/api/mercadoPagoRoute.js
@@ -2,41 +2,23 @@ require("dotenv").config();
 const router = require("express").Router();
 const mercadoPagoCheckout = require("../../controllers/payments/mercadoPagoCheckout");
 const mercadopago = require("mercadopago");
+const axios = require('axios');
 
-// router.post("/:id/:email/:description/:amount", async (req, res) => {
-//   mercadopago.configure({
-//     access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
-//   });
+console.log(mercadopago.payments);
 
-//   const preference = {
-//     items: [
-//       {
-//         title: "Test",
-//         quantity: 1,
-//         currency_id: "ARS",
-//         unit_price: 10.5,
-//       },
-//     ],
-//   };
-//   try {
-//     await mercadopago.preferences.create(preference);
-//   } catch (err) {
-//     return res.send(err.message);
-//   }
-// });
 
 router.get("/checkout/:id/:email/:description/:amount", mercadoPagoCheckout);
 
 router.get("/success", (req, res) => {
-  return res.render('success_screen')
+  return res.render("success_screen");
 });
 
 router.get("/pending", (req, res) => {
-  return res.render('pending_screen')
+  return res.render("pending_screen");
 });
 
 router.get("/failure", (req, res) => {
-  return res.render('failure_screen')
+  return res.render("failure_screen");
 });
 
 module.exports = router;
